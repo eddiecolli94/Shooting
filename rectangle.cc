ef91e65ec9b23889c0321409067cf44fd1a5174f
eddiecolli94 <60409357+eddiecolli94@users.noreply.github.com>
2022-02-08 20:09:01 -0800

    Fixed some parts
    Fixed some parts where ints were supposed to be doubles and added getters and setters for the solid. Also actually made the constructor this time.

--- rectangle.cc
+++ rectangle.cc
@@ -1,12 +1,34 @@
 #include <rectangle.h>
 
-int Rectangle::get_x_min() const{return x_min;}
-int Rectangle::get_y_min() const{return y_min;}
-int Rectangle::get_x_max() const{return x_max;}
-int Rectangle::get_y_max() const{return y_max;}
-void Rectangle::set_x_min(int new_x_min) {x_min = new_x_min;}
-void Rectangle::set_y_min(int new_y_min) {y_min = new_y_min;}
-void Rectangle::set_x_max(int new_x_max) {x_max = new_x_max;}
-void Rectangle::set_y_max(int new_y_max) {y_max = new_y_max;}
+void Rectangle::die() {
+	cout << "BAD INPUT!" << endl; 
+	exit(EXIT_FAILURE);
+}
+
+Rectangle::Rectangle() {
+	solid = 1; //assume the rectangle is solid as the default
+	x_min = 0;
+	y_min = 0;
+	x_max = 0;
+	y_max = 0;
+}
+Rectangle::Rectangle(int new_solid, double new_x_min, double new_y_min, double new_x_max, double new_y_max) {
+	if(new_solid != 0 or new_solid != 1) die(); //this line may need to be changed to allow for weird input
+	solid = new_solid;
+	x_min = new_x_min;
+	y_min = new_x_min;
+	x_max = new_x_max;
+	y_max = new_x_max;
+}
+int Rectangle::get_solid() const{return solid;}
+double Rectangle::get_x_min() const{return x_min;}
+double Rectangle::get_y_min() const{return y_min;}
+double Rectangle::get_x_max() const{return x_max;}
+double Rectangle::get_y_max() const{return y_max;}
+void Rectangle::set_solid(int new_solid) {solid = new_solid;}
+void Rectangle::set_x_min(double new_x_min) {x_min = new_x_min;}
+void Rectangle::set_y_min(double new_y_min) {y_min = new_y_min;}
+void Rectangle::set_x_max(double new_x_max) {x_max = new_x_max;}
+void Rectangle::set_y_max(double new_y_max) {y_max = new_y_max;}
 void Rectangle::push_children (int index) {vec.push_back(index);}
 //int Rectangle::get_child_index() {}
--- rectangle.h
+++ rectangle.h
@@ -2,20 +2,26 @@
 #include <iostream>
 using namespace std;
 
+void die();
+
 class Rectangle{
 	private:
 		int solid; //1 if solid, 0 if nonsolid
 		double x_min, y_min, x_max, y_max;
 		Vec<int> vec; //Vec will hold the index of the Rectangle's children
 	public:
-		int get_x_min() const;
-		int get_y_min() const;
-		int get_x_max() const;
-		int get_y_max() const;
-		void set_x_min(int new_x_min);
-		void set_y_min(int new_y_min);
-		void set_x_max(int new_x_max);
-		void set_y_max(int new_y_max);
+		Rectangle();
+		Rectangle(int new_solid, double new_x_min, double new_y_min, double new_x_max, double new_y_max);
+		int get_solid() const;
+		double get_x_min() const;
+		double get_y_min() const;
+		double get_x_max() const;
+		double get_y_max() const;
+		void set_solid(int new_solid);
+		void set_x_min(double new_x_min);
+		void set_y_min(double new_y_min);
+		void set_x_max(double new_x_max);
+		void set_y_max(double new_y_max);
 		void push_children(int index);
 		//int get_child_index();
 }
